841c299c1abda30e3d027ecff1d89f5374b3e515
bappy958 <itznobita958@gmail.com>
2025-09-26 17:20:22 +0600

    change settings.py file

--- gasLeckageDetector/settings.py
+++ gasLeckageDetector/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-_)v^nx8khyh^re$+2kk1kaofy4@ptbe38m_7=!lf7*uxyv9@1n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "gas-leak-detector.onrender.com"] # sob jayga theka request korta parba
+ALLOWED_HOSTS = ["*", "https://gas-leak-detector.onrender.com"] # sob jayga theka request korta parba
 
-CSRF_TRUSTED_ORIGINS = ["gas-leak-detector.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://gas-leak-detector.onrender.com"]
 
 
 
