e6261fc30e76220b5ec417b4303db656d71bd7e1
bappy958 <itznobita958@gmail.com>
2025-09-26 18:06:32 +0600

    modify view

--- alert/views.py
+++ alert/views.py
@@ -16,6 +16,40 @@
 
 
 
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from django.core.mail import EmailMultiAlternatives
+# from django.template.loader import render_to_string
+# from django.utils.html import strip_tags
+# from django.views.decorators.csrf import csrf_exempt
+
+
+# @csrf_exempt
+# @api_view(["POST"])
+# def send_alert(request):
+#     message = request.data.get("message", "No Message")
+#     subject = "🚨 Gas Leakage Alert"
+
+#     # Render HTML template (we’ll create it soon)
+#     html_content = render_to_string("emails/alert.html", {"message": message})
+#     text_content = strip_tags(html_content)  # fallback plain text
+
+#     email = EmailMultiAlternatives(
+#         subject,
+#         text_content,
+#         "dev.morsalin@gmail.com",  # sender
+#         ["weshallovercome335@gmail.com", "bappyawal0@gmail.com"],  # recipient
+#     )
+#     email.attach_alternative(html_content, "text/html")
+#     email.send()
+
+    
+
+#     return Response({"status": "Email Sent with HTML", "message": message})
+
+
+
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core.mail import EmailMultiAlternatives
@@ -23,14 +57,13 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.views.decorators.csrf import csrf_exempt
 
-
 @csrf_exempt
 @api_view(["POST"])
 def send_alert(request):
     message = request.data.get("message", "No Message")
     subject = "🚨 Gas Leakage Alert"
 
-    # Render HTML template (we’ll create it soon)
+    # Render the new HTML template
     html_content = render_to_string("emails/alert.html", {"message": message})
     text_content = strip_tags(html_content)  # fallback plain text
 
@@ -38,9 +71,9 @@ def send_alert(request):
         subject,
         text_content,
         "dev.morsalin@gmail.com",  # sender
-        ["weshallovercome335@gmail.com", "bappyawal0@gmail.com"],  # recipient
+        ["weshallovercome335@gmail.com"],  # recipient
     )
     email.attach_alternative(html_content, "text/html")
     email.send()
 
-    return Response({"status": "Email Sent with HTML", "message": message})
+    return Response({"status": "Email Sent", "message": message})
